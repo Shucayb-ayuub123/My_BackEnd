cfac21c620769f835638cd25460292b9620d6110
Shucayb-ayuub123 <shucayb594@gmail.com>
2025-12-29 19:56:24 +0300

    Added DB

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import AuthRoute from "./routes/Auth.js";
 import Task_OP from './routes/Operation.js'
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const PORT = 3000;
 
