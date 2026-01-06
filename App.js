bbd803bd6c95a8b816149258cb8e95b466c4cbee
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-01-06 20:25:24 +0300

    mm

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth", AuthRoute);
+app.use("/Auth", AuthRoute);
 app.use("/task", Task_OP);
 
 app.listen(PORT, () => {
