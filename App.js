0fce59d34d9fdf46c9fe08326ec29bf6c3b6ce43
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-01-10 17:35:37 +0300

    Just for

--- App.js
+++ App.js
@@ -17,7 +17,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/" , (req,res) => {
+  res.send("Just for testing")
+})
 app.use("/Auth", AuthRoute);
 app.use("/task", Task_OP);
 
