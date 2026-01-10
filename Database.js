98ffcc1af04798fdb47af7bf237bd176073a8d8e
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-01-10 20:54:20 +0300

    mm

--- Database.js
+++ Database.js
@@ -7,8 +7,7 @@ const Mysql = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port:  process.env.PORT
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
 });
 
-
-export default  Mysql
+export default Mysql
