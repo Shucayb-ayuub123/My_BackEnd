0fc0920819bf8252cec5b46393a9fe02b6c17623
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-01-11 09:39:55 +0300

    Backend ready for Railway

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
-}));
+}));    
 // Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
