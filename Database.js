7e03db5319e5a1108d30c022aeaa7458636c19ad
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-01-12 17:55:00 +0300

    mm

--- Database.js
+++ Database.js
@@ -6,15 +6,7 @@ const Mysql = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT),
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 10000, // 10 seconds
-  ssl: {
-    rejectUnauthorized: false
-  }
+  database: process.env.DB_DATABASE
 });
 Mysql.getConnection()
   .then(conn => {
